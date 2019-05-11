ec7431e3a242e6a5e45dfe5954a1ffdc976c32e3
neridev <rodrigogregorioneri@hotmail.com>
2019-05-11 19:46:59 -0300

    Commit inicial

---
+++ src/main/java/com/neri/EventNeriApplication.java
@@ -0,0 +1,13 @@
+package com.neri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventNeriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventNeriApplication.class, args);
+	}
+
+}
